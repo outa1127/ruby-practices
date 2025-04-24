2625706b9f097cbdcb63f79fc66f643927089eb3
outa1127 <ota.ono@livesense.co.jp>
2025-04-24 12:12:02 +0900

    不要なコメントを削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,26 +3,25 @@
 
 score = ARGV[0]
 
-# splitメソッドで引数で指定した','ごとに区切った新たな配列を生成する
 scores = score.split(',')
 STRIKE_SCORE = 10
 
 shots = []
 scores.each do |s|
-  if s == 'X' # X(ストライク)だったら配列に10と0を追加
+  if s == 'X'
     shots << STRIKE_SCORE
     shots << 0
   else
-    shots << s.to_i # それ以外は数値に変換して配列に追加
+    shots << s.to_i
   end
 end
 
 frames = shots.each_slice(2).to_a
 
-if frames.length == 12 # 配列が12だった場合
-  frames[-3..] = [frames[-3..].flatten] # 最後の三つの配列を結合
-elsif frames.length == 11 # 配列が11だった場合
-  frames[-2..] = [frames[-2..].flatten] # 最後の二つの配列を結合
+if frames.length == 12
+  frames[-3..] = [frames[-3..].flatten]
+elsif frames.length == 11
+  frames[-2..] = [frames[-2..].flatten]
 end
 
 def strike?(frame)
@@ -38,7 +37,6 @@ def spare?(frame)
 end
 
 point = 0
-# # 9フレームだけ他のフレームと挙動が異なる必要がある
 frames.each_with_index do |frame, i|
   point +=
     if (i < 8)
@@ -46,7 +44,7 @@ frames.each_with_index do |frame, i|
         frame.sum + frames[i + 1].sum + frames[i + 2][0]
       elsif strike?(frame) && not_strike?(frames[i + 1])
         frame.sum + frames[i + 1].sum
-      elsif spare?(frame) # 8フレーム以下かつスペア
+      elsif spare?(frame)
         frame.sum + frames[i + 1][0]
       else
         frame.sum
