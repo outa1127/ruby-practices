c11bd3c54c7365eddda3ff6861ccbdd1f206a844
outa1127 <ota.ono@livesense.co.jp>
2025-04-28 12:01:45 +0900

    不要なコメントを削除

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 current_dir_contents = Dir.glob("*", File::FNM_DOTMATCH)
-sliced_contents = current_dir_contents.each_slice(3).to_a # 表示する列数を修正するときはここの数字を変更
+sliced_contents = current_dir_contents.each_slice(3).to_a
+rows = sliced_contents.length
+cols = sliced_contents.length >= 1 ? sliced_contents.first.length : 0
+index_contents = []
 
-rows = sliced_contents.length # 配列の数。[[1,2],[3,4],[5,6]]だったら3
-cols = sliced_contents.length >= 1 ? sliced_contents.first.length : 0 # 配列の中の要素の数。上の例で言うと2
-index_contents = [] # 修正後の配列を格納する新たな配列
-
-# 以下の繰り返し処理を実行することで、sliced_contents[0~3][0]の要素、sliced_contents[0~3][１]みたいな感じで配列を再生成できる。
 cols.times do |col|
   index_rows = []
   rows.times do |row|
@@ -16,9 +14,9 @@ cols.times do |col|
   index_contents << index_rows
 end
 
-index_contents.each do |inner_array|
-  formatted_line = inner_array.compact.map do |element| # compactメソッドを使用することでnilの配列を排除してmap関数を実行することができる
-    element.ljust(15) # 左よせ15文字文のスペースでファイルを表示するようにする
+index_contents.each do |index_content|
+  formatted_contents = index_content.compact.map do |element|
+    element.ljust(15)
   end
-  puts formatted_line.join
+  puts formatted_contents.join
 end
