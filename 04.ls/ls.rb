a42d3776b82b64e52128b10f30bcea8623585384
outa1127 <ota.ono@livesense.co.jp>
2025-04-30 11:13:49 +0900

    インデックスされた配列のヘンス名を修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,17 +4,17 @@ current_dir_contents = Dir.glob("*", File::FNM_DOTMATCH)
 sliced_contents = current_dir_contents.each_slice(3).to_a
 rows = sliced_contents.length
 cols = sliced_contents.length >= 1 ? sliced_contents.first.length : 0
-index_contents = []
+indexed_contents = []
 
 cols.times do |col|
   index_rows = []
   rows.times do |row|
     index_rows << sliced_contents[row][col]
   end
-  index_contents << index_rows
+  indexed_contents << index_rows
 end
 
-formatted_contents = index_contents.map do |index_content|
+formatted_contents = indexed_contents.map do |index_content|
   index_content.compact.map do |element|
     element.ljust(15)
   end.join
