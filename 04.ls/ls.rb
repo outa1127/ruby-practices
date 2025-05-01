202a3528eb496dc558c413ba31d271762e454465
outa1127 <ota.ono@livesense.co.jp>
2025-05-01 10:40:50 +0900

    lsコマンドを作る2

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+opt = OptionParser.new
+opt.on('-a') { puts '-a option' }
+opt.parse(ARGV)
+
 current_dir_contents = Dir.glob("*", File::FNM_DOTMATCH)
 cols = 3
 rows = current_dir_contents.length >= 1 ? (current_dir_contents.size.to_f / cols).ceil : 0 # 5
