93301633f3204227ae394612a86fbf0b4b0a06ce
outa1127 <ota.ono@livesense.co.jp>
2025-05-01 10:15:58 +0900

    lsコマンドの作成 (#4)
    * lsコマンドの作成
    
    * コメントの追加
    
    * rubocopの修正を反映
    
    * 左よせするスペースを修正
    
    * Dir.globメソッドを使用するように修正
    
    * 三項演算子でcols変数に代入するように修正
    
    * 配列の変数名を修正
    
    * 配列の名称を再修正
    
    * 不要なコメントを削除
    
    * 要素を出力するロジックを修正
    
    * インデックスされた配列のヘンス名を修正
    
    * 配列の作り方を修正
    
    * 3列で表示するように修正

---
+++ 04.ls/ls.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+current_dir_contents = Dir.glob("*", File::FNM_DOTMATCH)
+cols = 3
+rows = current_dir_contents.length >= 1 ? (current_dir_contents.size.to_f / cols).ceil : 0 # 5
+sliced_contents = current_dir_contents.each_slice(rows).to_a
+
+indexed_contents = Array.new(rows) do |row|
+  Array.new(cols) do |col|
+    sliced_contents[col][row]
+  end
+end
+
+formatted_contents = indexed_contents.map do |index_content|
+  index_content.compact.map do |element|
+    element.ljust(15)
+  end.join
+end
+puts formatted_contents
