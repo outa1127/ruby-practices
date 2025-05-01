76a02b95762a892273cdf40416bd9e416cb66c1a
outa1127 <ota.ono@livesense.co.jp>
2025-05-01 10:16:17 +0900

    ボウリングプログラムの作成 (#3)
    * ボウリングプログラムの作成
    
    * ストライクを取った時の挙動を修正
    
    * コメントを修正
    
    * ストライクとスペアを判定するメソッドの追加
    
    * 配列をフレームごとに分割する方法を修正
    
    * 配列の結合をflattenを使用して行う
    
    * フレーム数に応じた条件分岐を修正
    
    * 不要なコメントを削除
    
    * 複数回frame.sumを行わないように修正
    
    * ボーナススコアがなかった時の変数名を修正
    
    * 条件式の不要な括弧を削除
    
    * spareを判定するメソッドを修正
    
    * 基本点の算出方法を修正
    
    * ボーナスが加算される時のプログラムを修正
    
    * 10番目以降の配列を結合する方法を修正
    
    * ベースポイントをpoint変数に加算するように修正
    
    * スペアの条件式を修正

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+score = ARGV[0]
+
+scores = score.split(',')
+STRIKE_SCORE = 10
+STRIKE_SECOND_THROW_SCORE = 0
+
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << STRIKE_SCORE
+    shots << STRIKE_SECOND_THROW_SCORE
+  else
+    shots << s.to_i
+  end
+end
+
+frames = shots.each_slice(2).to_a
+
+frames[9..] = [frames[9..].flatten]
+
+def strike?(frame)
+  frame[0] == STRIKE_SCORE
+end
+
+def not_strike?(frame)
+  frame[0] != STRIKE_SCORE
+end
+
+def spare?(frame)
+  not_strike?(frame) && frame.sum == STRIKE_SCORE
+end
+
+point = 0
+
+# frames.sum { |frame| frame.sum}と同じ意味
+point += frames.sum(&:sum)
+
+NO_BONUS_SCORE = 0
+
+frames.each_with_index do |frame, i|
+  next if i >= 9
+
+  point +=
+    if strike?(frame) && strike?(frames[i + 1])
+      if i == 8
+        frames[i + 1][0] + frames[i + 1][2]
+      else
+        frames[i + 1][0] + frames[i + 2][0]
+      end
+    elsif strike?(frame) && not_strike?(frames[i + 1])
+      frames[i + 1][0] + frames[i + 1][1]
+    elsif spare?(frame)
+      frames[i + 1][0]
+    else
+      NO_BONUS_SCORE
+    end
+end
+
+puts point
