decaf08a18d0ba9d7df313184f9dfaf126131305
outa1127 <ota.ono@livesense.co.jp>
2025-05-08 15:51:01 +0900

    メソッドの引数に直接ディレクトリの情報を入れるように修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,31 +4,10 @@ require 'optparse'
 
 option_used = false
 
-def no_option
-  current_dir_contents = Dir.glob('*')
+def formatting_contents(dir_contents)
   cols = 3
-  rows = current_dir_contents.length >= 1 ? (current_dir_contents.size.to_f / cols).ceil : 0 # 5
-  sliced_contents = current_dir_contents.each_slice(rows).to_a
-
-  indexed_contents = Array.new(rows) do |row|
-    Array.new(cols) do |col|
-      sliced_contents[col][row]
-    end
-  end
-
-  formatted_contents = indexed_contents.map do |index_content|
-    index_content.compact.map do |element|
-      element.ljust(15)
-    end.join
-  end
-  puts formatted_contents
-end
-
-def a_option
-  current_dir_contents = Dir.glob('*', File::FNM_DOTMATCH)
-  cols = 3
-  rows = current_dir_contents.length >= 1 ? (current_dir_contents.size.to_f / cols).ceil : 0 # 5
-  sliced_contents = current_dir_contents.each_slice(rows).to_a
+  rows = dir_contents.length >= 1 ? (dir_contents.size.to_f / cols).ceil : 0 # 5
+  sliced_contents = dir_contents.each_slice(rows).to_a
 
   indexed_contents = Array.new(rows) do |row|
     Array.new(cols) do |col|
@@ -47,9 +26,9 @@ end
 OptionParser.new do |opt|
   opt.on('-a') do
     option_used = true
-    a_option
+    formatting_contents(Dir.glob('*', File::FNM_DOTMATCH))
   end
   opt.parse!(ARGV)
 end
 
-no_option unless option_used
+formatting_contents(Dir.glob('*')) unless option_used
