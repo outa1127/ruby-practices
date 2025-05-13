244b90b93c46d2be2ed0c78fc9c155b8a4834ad2
outa1127 <ota.ono@livesense.co.jp>
2025-05-13 10:43:06 +0900

    lsコマンドを作る2 (#5)
    * lsコマンドを作る2
    
    * オプションの有無で実行するメソッドを分岐
    
    * ダブルクォーテーションマークを使用しないように修正
    
    * 隠しファイルを削除するプログラムを修正
    
    * メソッドの引数に直接ディレクトリの情報を入れるように修正
    
    * flags変数を定義し、メソッドを一度だけ呼び出すように修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,19 +1,33 @@
 # frozen_string_literal: true
 
-current_dir_contents = Dir.glob("*", File::FNM_DOTMATCH)
-cols = 3
-rows = current_dir_contents.length >= 1 ? (current_dir_contents.size.to_f / cols).ceil : 0 # 5
-sliced_contents = current_dir_contents.each_slice(rows).to_a
+require 'optparse'
 
-indexed_contents = Array.new(rows) do |row|
-  Array.new(cols) do |col|
-    sliced_contents[col][row]
+def formatting_contents(dir_contents)
+  cols = 3
+  rows = dir_contents.length >= 1 ? (dir_contents.size.to_f / cols).ceil : 0 # 5
+  sliced_contents = dir_contents.each_slice(rows).to_a
+
+  indexed_contents = Array.new(rows) do |row|
+    Array.new(cols) do |col|
+      sliced_contents[col][row]
+    end
   end
+
+  formatted_contents = indexed_contents.map do |index_content|
+    index_content.compact.map do |element|
+      element.ljust(15)
+    end.join
+  end
+  puts formatted_contents
 end
 
-formatted_contents = indexed_contents.map do |index_content|
-  index_content.compact.map do |element|
-    element.ljust(15)
-  end.join
+flags = 0
+
+OptionParser.new do |opt|
+  opt.on('-a') do
+    flags |= File::FNM_DOTMATCH
+  end
+  opt.parse!(ARGV)
 end
-puts formatted_contents
+
+formatting_contents(Dir.glob('*', flags))
